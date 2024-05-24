5bb9b47176f567fe453f4b3e74964b1423435ec9
Brianwu24 <brian.wu24@ycdsbk12.ca>
2024-05-24 13:54:22 -0400

    Made the map generator object and added circles as a tree placeholder. Reorganized files and renamed file names.

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class inital_script : MonoBehaviour
+public class GameController : MonoBehaviour
 {
+    public GameObject player;
     // Start is called before the first frame update
     void Start()
     {
---
+++ Assets/MapGenerator.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using MathF = System.MathF;
+using Random = Unity.Mathematics.Random;
+
+public class MapGenerator : MonoBehaviour
+{
+    private Random _rng;
+
+    public GameObject tree;
+    private List<GameObject> _treeList;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this._rng = new Random((uint)UnityEngine.Random.Range(1, 100000));
+        _treeList = new List<GameObject>();
+        for (int i=0; i< 20; i++)
+        {
+            _treeList.Add(Instantiate(tree, new Vector3(this._rng.NextFloat(-10, 10), _rng.NextFloat(-10, 10)), Quaternion.identity, this.transform));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Debug.Log(_rng.NextFloat(0, 2.4f));
+    }
+}

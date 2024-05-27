f50f9d4ed8a53e9becc7c191f5c9533e0e5a0ff1
Brianwu24 <brian.wu24@ycdsbk12.ca>
2024-05-27 16:55:40 -0400

    Updated map generation techniques (chunking) (not fully complete). Added more methods to the game controller so that it can be used with map controller.

--- Assets/MapController.cs
+++ Assets/MapController.cs
@@ -5,21 +5,64 @@ using UnityEngine;
 using MathF = System.MathF;
 using Random = Unity.Mathematics.Random;
 
-public class MapGenerator : MonoBehaviour
+public class MapChunk
 {
     private Random _rng;
+    // private List<GameObject> _treeList;
+    private Vector3 _startingPos;
+    private int _numTrees;
 
-    public GameObject tree;
-    private List<GameObject> _treeList;
+    public MapChunk(List<GameObject> mapObjects, Transform parentTransform, Vector3 startingPos, int numTrees, float size = 10.0f)
+    {
+        
+        this._rng = new Random((uint)UnityEngine.Random.Range(1, 100000));
+        _startingPos = startingPos;
+        _numTrees = numTrees;
+        // _startingPos += new Vector3();
+        
+        
+        for (int i=0; i< size; i++)
+        {
+            // this._treeList.Add(GameObject.Instantiate(tree, _startingPos + new Vector3(), Quaternion.identity, parentTransform));
+            GameObject.Instantiate(mapObjects[0], _startingPos + new Vector3(this._rng.NextFloat(-size / 2, size / 2), this._rng.NextFloat(-size / 2, size / 2)), Quaternion.identity, parentTransform);
+        }
+    }
+}
+public class MapController : MonoBehaviour
+{
+    public GameObject gameController;
+    private GameController _gameController;
+    private Player _player;
+    private Random _rng;
+
+    public GameObject mapObjects;
+
+    private List<GameObject> _mapObjects;
+
+    private MapChunk _chunk; 
+    // private List<MapChunk> _chunks;
     // Start is called before the first frame update
+    
+    // private void 
     void Start()
     {
-        this._rng = new Random((uint)UnityEngine.Random.Range(1, 100000));
-        _treeList = new List<GameObject>();
-        for (int i=0; i< 20; i++)
+        _gameController = gameController.GetComponent<GameController>();
+        _player = _gameController.GetComponent<Player>();
+        
+        _mapObjects = new List<GameObject>();
+        foreach (Transform childObject in mapObjects.transform)
         {
-            _treeList.Add(Instantiate(tree, new Vector3(this._rng.NextFloat(-10, 10), _rng.NextFloat(-10, 10)), Quaternion.identity, this.transform));
+            _mapObjects.Add(childObject.gameObject);
         }
+
+        MapChunk _chunk = new MapChunk(_mapObjects, this.transform, new Vector3(), 5);
+        // _chunks.Add(new Chunk(tree, this.transform, new Vector3(), 1, 5));
+        // this._rng = new Random((uint)UnityEngine.Random.Range(1, 100000));
+        // _treeList = new List<GameObject>();
+        // for (int i=0; i< 20; i++)
+        // {
+        //     _treeList.Add(Instantiate(tree, new Vector3(this._rng.NextFloat(-10, 10), _rng.NextFloat(-10, 10)), Quaternion.identity, this.transform));
+        // }
     }
 
     public void Move(Vector3 translation)
--- Assets/player.cs
+++ Assets/player.cs
@@ -2,9 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class player : MonoBehaviour
+public class Player : MonoBehaviour
 {
+    public GameObject gameController;
+    private GameController _gameController;
+    
+    public GameObject mapController;
+    private MapController _mapController;
 
+    // private MapController _mapController;
     //movement
     public float moveSpeed;
     Rigidbody2D rb;
@@ -13,7 +19,24 @@ public class player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        _gameController = gameController.GetComponent<GameController>();
+        _mapController = mapController.GetComponent<MapController>();
+        // rb = GetComponent<Rigidbody2D>();
+        // _mapController = mapController.GetComponent<MapController>();
+    }
+    
+    void InputManagement()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        moveDir = new Vector2(moveX, moveY).normalized;
+    }
+
+    private Vector3 GetMotion()
+    {
+        return new Vector3(moveDir.x * moveSpeed, moveDir.y * moveSpeed);
+        // rb.velocity = new Vector2(moveDir.x * moveSpeed, moveDir.y * moveSpeed);
     }
 
     // Update is called once per frame
@@ -24,20 +47,10 @@ public class player : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Move();
+        _gameController.Move(this.GetMotion());
+        // _mapController.Move(this.GetMotion());
+        
     }
-
-    void InputManagement()
-    {
-        float moveX = Input.GetAxisRaw("Horizontal");
-        float moveY = Input.GetAxisRaw("Vertical");
-
-        moveDir = new Vector2(moveX, moveY).normalized;
-    }
-
-    void Move()
-    {
-        rb.velocity = new Vector2(moveDir.x * moveSpeed, moveDir.y * moveSpeed);
-    }
+    
 }
 

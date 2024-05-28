51420a2e03b9a0efe3d2bc3a6958550d5d989b8b
Brianwu24 <brian.wu24@ycdsbk12.ca>
2024-05-28 12:08:16 -0400

    Reverted player.cs

--- Assets/player.cs
+++ Assets/player.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -7,10 +5,6 @@ public class Player : MonoBehaviour
     public GameObject gameController;
     private GameController _gameController;
     
-    public GameObject mapController;
-    private MapController _mapController;
-
-    // private MapController _mapController;
     //movement
     public float moveSpeed;
     Rigidbody2D rb;
@@ -20,9 +14,7 @@ public class Player : MonoBehaviour
     void Start()
     {
         _gameController = gameController.GetComponent<GameController>();
-        _mapController = mapController.GetComponent<MapController>();
-        // rb = GetComponent<Rigidbody2D>();
-        // _mapController = mapController.GetComponent<MapController>();
+        rb = GetComponent<Rigidbody2D>();
     }
     
     void InputManagement()
@@ -33,10 +25,9 @@ public class Player : MonoBehaviour
         moveDir = new Vector2(moveX, moveY).normalized;
     }
 
-    private Vector3 GetMotion()
+    private void Move()
     {
-        return new Vector3(moveDir.x * moveSpeed, moveDir.y * moveSpeed);
-        // rb.velocity = new Vector2(moveDir.x * moveSpeed, moveDir.y * moveSpeed);
+        rb.velocity = new Vector2(moveDir.x * moveSpeed, moveDir.y * moveSpeed);
     }
 
     // Update is called once per frame
@@ -47,8 +38,7 @@ public class Player : MonoBehaviour
 
     private void FixedUpdate()
     {
-        _gameController.Move(this.GetMotion());
-        // _mapController.Move(this.GetMotion());
+        Move();
         
     }
     

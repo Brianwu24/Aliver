018e49df4dbed4d4451bb000fc0e1b47e5561fd0
Brianwu24 <brian.wu24@ycdsbk12.ca>
2024-05-28 15:15:01 -0400

    Implemented trees generation with perlin noise. Note no sprite used.

--- Assets/MapController.cs
+++ Assets/MapController.cs
@@ -11,25 +11,32 @@ public class PerlinNoise
     public float orgX;
     public float orgY;
     
-
     private float[][] _noise;
-    private float _scale;
 
-    public PerlinNoise(int width, int height, float scale=0.3f)
+    public PerlinNoise(int width, int height, float scale=0.5f)
     {
-        _scale = scale;
-        _noise = new float[height][];
+        _noise = new float[(int)(height / scale)][];
         for (int i = 0; i < _noise.Length; i++)
         {
-            _noise[i] = new float[width];
+            _noise[i] = new float[(int)(width / scale)];
         }
-        for (float y = 0.0F; y < height; y++)
+
+        float sum_samples = 0;
+        for (float y = 0.0F; y < _noise.Length; y++)
         {
-            for (float x = 0.0F; x < width; x++)
+            for (float x = 0.0F; x < (int)(width / scale); x++)
             {
-                float sample = Mathf.PerlinNoise(orgX + x * scale, orgY + y * _scale);
-                Debug.Log((x, y));
+                float sample = Mathf.PerlinNoise(orgX + x * scale, orgY + y * scale);
                 _noise[(int)y][(int)x] = sample;
+                sum_samples += sample;
+            }
+        }
+
+        foreach (var row in _noise)
+        {
+            for (int x = 0; x < row.Length; x++)
+            {
+                row[x] /= sum_samples;
             }
         }
     }
@@ -58,19 +65,47 @@ public class MapController : MonoBehaviour
     private Random _rng;
 
     public GameObject mapObjects;
-    private PerlinNoise _mapNoise;
-
     private List<GameObject> _mapObjects;
-    
-    
+    private List<GameObject> _trees;
+
+    private float[][] _mapNoise;
+
+
+    public void CreateTrees(float[][] noise, float scale)
+    {
+        for (int y = 0; y < _mapNoise.GetLength(0); y++)
+        {
+            int x = 0;
+            foreach (float probability in noise[y])
+            {
+                if (probability + 0.04>= _rng.NextFloat(0f, 1f))
+                {
+                    Instantiate(_trees[0], new Vector3(x * scale - 10 + _rng.NextFloat(-1, 1), y * scale - 6 + _rng.NextFloat(-1, 1)), Quaternion.identity,
+                        this.transform);
+                }
+                x++;
+            }
+        }
+    }
+
+
     // private void 
     void Start()
     {
+        _rng = new Random((uint)UnityEngine.Random.Range(1, 100000));
         _gameController = gameController.GetComponent<GameController>();
         _player = _gameController.GetComponent<Player>();
 
-        _mapNoise = new PerlinNoise(10, 6);
-        _mapNoise.PrintNoise();
+
+        _mapObjects = new List<GameObject>();
+        foreach (Transform child in mapObjects.transform)
+        {
+            _mapObjects.Add(child.gameObject);
+        }
+
+        float scale = 0.5f;
+        _mapNoise = new PerlinNoise(20, 12, scale).GetPerlinNoise();
+        CreateTrees(_mapNoise, scale);
 
     }
 

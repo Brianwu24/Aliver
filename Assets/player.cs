12ede3a210202494b1220f3c734915151bc33323
NeSteat <Ethanx5577@gmail.com>
2024-06-09 18:29:59 -0400

    Added a shop

--- Assets/player.cs
+++ Assets/player.cs
@@ -16,6 +16,7 @@ public class Player : MonoBehaviour
     private Rigidbody2D _rb;
     private Vector2 _moveDir;
     private Transform _transform;
+    public float speed = 6f;
 
     // Start is called before the first frame update
     void Start()

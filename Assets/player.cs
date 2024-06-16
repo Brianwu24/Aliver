d78600941e81048e2f5e14e0afad6ac188e0168d
Brianwu24 <brian.wu24@ycdsbk12.ca>
2024-06-16 16:17:32 -0400

    Updated player, not sure, this should have been updated in the previous commit. Ignore this

--- Assets/player.cs
+++ Assets/player.cs
@@ -1,3 +1,4 @@
+using System;
 using Unity.Mathematics;
 using UnityEngine;
 
@@ -9,7 +10,9 @@ public class Player : MonoBehaviour
     public GameObject enemyManager;
     private EnemyManager _enemyManager;
 
-    public GameObject bulletObject;
+    public GameObject basicBulletObject;
+    public GameObject advancedBulletObject;
+    public GameObject expertBulletObject;
     private Bullet _bullet;
     
     //movement
@@ -18,15 +21,32 @@ public class Player : MonoBehaviour
     private Transform _transform;
     public float speed = 6f;
 
+    private float _health;
+
     // Start is called before the first frame update
     void Start()
     {
         _gameController = gameController.GetComponent<GameController>();
         _rb = GetComponent<Rigidbody2D>();
 
-        _bullet = bulletObject.GetComponent<Bullet>();
+        string playerBulletType = _gameController.GetBulletType();
+        if (playerBulletType == "Basic")
+        {
+            _bullet = basicBulletObject.GetComponent<Bullet>();
+        }
+        else if (playerBulletType == "Advanced")
+        {
+            _bullet = advancedBulletObject.GetComponent<Bullet>();
+        }        
+        else if (playerBulletType == "Expert")
+        {
+            _bullet = expertBulletObject.GetComponent<Bullet>();
+        }
+        
         _enemyManager = enemyManager.GetComponent<EnemyManager>();
         _transform = GetComponent<Transform>();
+
+        _health = 20f;
     }
     
     void InputManagement()
@@ -65,11 +85,24 @@ public class Player : MonoBehaviour
         // }
         // Debug.Log(directionVector);
         
-        GameObject newBullet = Instantiate(bulletObject, _transform.position, Quaternion.identity, _transform);
-        
+        GameObject newBullet = Instantiate(basicBulletObject, _transform.position, Quaternion.identity, _transform);
+        newBullet.SetActive(true);
         // newBullet.GetComponent<Bullet>().SetDirectionSpeed(directionVector * _gameController.GetBulletSpeed());
     }
-    
+
+    public void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            _health -= other.gameObject.GetComponent<EnemyController>().GetDamage();
+            Debug.Log(_health);
+            if (_health <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
     void Update()
     {
         InputManagement();

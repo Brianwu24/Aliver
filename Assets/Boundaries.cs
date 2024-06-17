6b72e6f8bed4f86220a1a1acf19f63f85610eaf7
PlentifulPotato31 <CanadienSyrup@gmail.com>
2024-06-17 12:00:42 -0400

    Added proper encapsulation to Boundaries script

--- Assets/Boundaries.cs
+++ Assets/Boundaries.cs
@@ -4,23 +4,23 @@ using UnityEngine;
 
 public class Boundaries : MonoBehaviour {
     public Camera MainCamera;
-    private Vector2 screenBounds;
-    private float objectWidth;
-    private float objectHeight;
-    private float xMargin = 6f;
+    private Vector2 _screenBounds;
+    private float _objectWidth;
+    private float _objectHeight;
+    private float _xMargin = 6f;
 
     // Use this for initialization
     void Start () {
-        screenBounds = MainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, MainCamera.transform.position.z));
-        objectWidth = transform.GetComponent<SpriteRenderer>().bounds.extents.x; //extents = size of width / 2
-        objectHeight = transform.GetComponent<SpriteRenderer>().bounds.extents.y; //extents = size of height / 2
+        _screenBounds = MainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, MainCamera.transform.position.z));
+        _objectWidth = transform.GetComponent<SpriteRenderer>().bounds.extents.x; //extents = size of width / 2
+        _objectHeight = transform.GetComponent<SpriteRenderer>().bounds.extents.y; //extents = size of height / 2
     }
 
     // Update is called once per frame
     void LateUpdate(){
         Vector3 viewPos = transform.position;
-        viewPos.x = Mathf.Clamp(viewPos.x, screenBounds.x * -1 + objectWidth + xMargin, screenBounds.x - objectWidth - xMargin);
-        viewPos.y = Mathf.Clamp(viewPos.y, screenBounds.y * -1 + objectHeight, screenBounds.y - objectHeight);
+        viewPos.x = Mathf.Clamp(viewPos.x, _screenBounds.x * -1 + _objectWidth + _xMargin, _screenBounds.x - _objectWidth - _xMargin);
+        viewPos.y = Mathf.Clamp(viewPos.y, _screenBounds.y * -1 + _objectHeight, _screenBounds.y - _objectHeight);
         transform.position = viewPos;
     }
 }

b5ecee0d3972742bf4f054d9d42a8af6d0882b56
PlentifulPotato31 <CanadienSyrup@gmail.com>
2024-06-17 13:07:49 -0400

    Implemented a preferences screen with keybinds

---
+++ Assets/Keybind Scripts/KeybindAttack.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using TMPro;
+
+public class KeybindAttack : MonoBehaviour
+{
+    public TextMeshProUGUI buttonLbl;
+
+    private void Start() {
+        buttonLbl.text = PlayerPrefs.GetString("CustomKey");
+    }
+
+    private void Update() {
+        if (buttonLbl.text == "Awaiting Input") {
+            foreach (KeyCode keycode in Enum.GetValues(typeof(KeyCode))) {
+                if (Input.GetKey(keycode)) {
+                    buttonLbl.text = keycode.ToString();
+                    PlayerPrefs.SetString("CustomKey", keycode.ToString());
+                    PlayerPrefs.Save();
+                }
+            }
+        }
+    }
+
+    public void ChangeKey() {
+        buttonLbl.text = "Awaiting Input";
+    }
+}
